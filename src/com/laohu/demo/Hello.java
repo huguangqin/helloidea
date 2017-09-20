1549e78aea0a0a33c58de26390750308cbdb7312
unknown <>
2017-09-20 19:13:31 +0800

    Initial commit

---
+++ src/com/laohu/demo/Hello.java
@@ -0,0 +1,10 @@
+package com.laohu.demo;
+
+/**
+ * Created by HuGuangqin on 2017/9/20.
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.print("hello idea");
+    }
+}
